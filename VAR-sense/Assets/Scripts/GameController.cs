75d0235c660b4d62be05b36c02b6c3d8cb727c44
iosdev474 <iosdev474@icloud.com>
2018-01-27 06:39:40 +0530

    map and menu linked
    map and menu linked

--- VAR-sense/Assets/Scripts/GameController.cs
+++ VAR-sense/Assets/Scripts/GameController.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
+	[Range(0.00001f,3)]
 	float speed = 0.1f;
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		transform.Translate (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+		Vector3 velocity = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical")) * speed;
+		transform.Translate (velocity);
+		float rotation = 0;
+		if (Input.GetKey (KeyCode.Q)) {
+			rotation -= 5;
+		}
+		if (Input.GetKey (KeyCode.E)) {
+			rotation += 5;
+		}
+		transform.Rotate (0, rotation, 0);
 	}
 }
